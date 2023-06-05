c5b196d95d45eaa89653d453131bd07b19a8e065
srehan1 <syed.rehan@shoppertrak.com>
2023-06-05 21:25:41 +0530

    Attempting to minimize gas cost

--- contracts/FundMeV2.sol
+++ contracts/FundMeV2.sol
@@ -12,16 +12,16 @@ contract FundMeV2 {
     // --- State variables ---
     uint256 public constant MIN_VALUE = 1000 wei;
 
-    address[] public funders;
-    mapping(address => uint256) public addressToMoneyFunded;
+    address[] public s_funders;
+    mapping(address => uint256) public s_addressToMoneyFunded;
 
-    address public immutable owner;
+    address public immutable i_owner;
 
     // --- Modifiers ---
 
     modifier managerOnly() {
         // require(msg.sender == owner, "Only manager can withdraw funds.");
-        if (msg.sender != owner) {
+        if (msg.sender != i_owner) {
             revert FundMeV2__NotOwner();
         }
         _;
@@ -30,7 +30,7 @@ contract FundMeV2 {
     // --- Constructor ---
 
     constructor() {
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     receive() external payable {
@@ -48,8 +48,8 @@ contract FundMeV2 {
      */
     function fund() public payable {
         require(msg.value >= MIN_VALUE, "Minimum 1000 wei is required.");
-        funders.push(msg.sender);
-        addressToMoneyFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+        s_addressToMoneyFunded[msg.sender] += msg.value;
     }
 
     /**
@@ -58,14 +58,14 @@ contract FundMeV2 {
     function withdraw() public managerOnly {
         for (
             uint256 funderIndex = 0;
-            funderIndex < funders.length;
+            funderIndex < s_funders.length;
             funderIndex += 1
         ) {
-            address funder = funders[funderIndex];
-            addressToMoneyFunded[funder] = 0;
+            address funder = s_funders[funderIndex];
+            s_addressToMoneyFunded[funder] = 0;
         }
 
-        funders = new address[](0);
+        s_funders = new address[](0);
 
         // Using transfer()
         // payable(msg.sender).transfer(address(this).balance);
@@ -75,6 +75,25 @@ contract FundMeV2 {
         // require(sendStatus, "Withdrawal failed.");
 
         // Using call()
+        (bool isWithdrawalSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(isWithdrawalSuccess, "Withdrawal failed.");
+    }
+
+    function withdrawV2() public payable managerOnly {
+        address[] memory funders = s_funders;
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex += 1
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToMoneyFunded[funder] = 0;
+        }
+
+        s_funders = new address[](0);
+
         (bool isWithdrawalSuccess, ) = payable(msg.sender).call{
             value: address(this).balance
         }("");
--- test/unit/FundMe.Test.js
+++ test/unit/FundMe.Test.js
@@ -14,7 +14,7 @@ describe("Fund-me", async function () {
 
   describe("When contract is created", async function () {
     it("Contract owner is set", async function () {
-      const contractOwner = await fundMe.owner();
+      const contractOwner = await fundMe.i_owner();
       assert.equal(contractOwner, deployer);
     });
   });
@@ -28,13 +28,13 @@ describe("Fund-me", async function () {
 
     it("Should update sender's amount funded balance if minimum value or more is sent", async function () {
       await fundMe.fund({ value: ethers.utils.parseUnits("1000", "wei") });
-      const response = await fundMe.addressToMoneyFunded(deployer);
+      const response = await fundMe.s_addressToMoneyFunded(deployer);
       assert.equal(response.toString(), ethers.utils.parseUnits("1000", "wei"));
     });
 
     it("Should add funders in the list if minimum amount or more is sent", async function () {
       await fundMe.fund({ value: ethers.utils.parseUnits("1000", "wei") });
-      const newFunder = await fundMe.funders(0);
+      const newFunder = await fundMe.s_funders(0);
       assert.equal(newFunder, deployer);
     });
   });
@@ -88,10 +88,13 @@ describe("Fund-me", async function () {
 
       // Assert
 
-      await expect(fundMe.funders(0)).to.be.reverted;
+      await expect(fundMe.s_funders(0)).to.be.reverted;
 
       for (let i = 1; i < 3; i++) {
-        assert.equal(await fundMe.addressToMoneyFunded(accounts[i].address), 0);
+        assert.equal(
+          await fundMe.s_addressToMoneyFunded(accounts[i].address),
+          0
+        );
       }
     });
 
@@ -102,6 +105,60 @@ describe("Fund-me", async function () {
       await expect(attackerWithdraws.withdraw()).to.be.rejectedWith(
         "FundMeV2__NotOwner"
       );
+    });
+
+    it("(withdrawV2) Should allow withdraw from single funder", async function () {
+      // Arrange
+      const contractBalance = await fundMe.provider.getBalance(fundMe.address);
+      const funderBalance = await fundMe.provider.getBalance(deployer);
+
+      // Act
+      const txResponse = await fundMe.withdrawV2();
+      const txResponseReceipt = await txResponse.wait(1);
+      const { gasUsed, effectiveGasPrice } = txResponseReceipt;
+      const gasCost = gasUsed.mul(effectiveGasPrice);
+
+      const contractBalanceAfterTx = await fundMe.provider.getBalance(
+        fundMe.address
+      );
+      const funderBalanceAfterTx = await fundMe.provider.getBalance(deployer);
+
+      // Assert
+      assert.equal(contractBalanceAfterTx, 0);
+      assert.equal(
+        contractBalance.add(funderBalance).toString(),
+        funderBalanceAfterTx.add(gasCost).toString()
+      );
+    });
+
+    it("(withdrawV2) Allows to withdraw money contributed by multiple users", async function () {
+      // Arrange
+      const accounts = await ethers.getSigners();
+
+      for (let i = 1; i < 3; i++) {
+        const fundMeConnectedContract = await fundMe.connect(accounts[i]);
+        fundMeConnectedContract.fund({
+          value: ethers.utils.parseUnits("1000", "wei"),
+        });
+      }
+
+      const contractBalance = await fundMe.provider.getBalance(fundMe.address);
+      const deployerBalance = await fundMe.provider.getBalance(deployer);
+
+      // Act
+      const txResponse = await fundMe.withdrawV2();
+      const txResponseReceipt = await txResponse.wait(1);
+
+      // Assert
+
+      await expect(fundMe.s_funders(0)).to.be.reverted;
+
+      for (let i = 1; i < 3; i++) {
+        assert.equal(
+          await fundMe.s_addressToMoneyFunded(accounts[i].address),
+          0
+        );
+      }
     });
   });
 });
